85dd8012827c053a131f6fe7e00f2ee6e3c9a929
Vyacheslav Kuznetsov <stripslash@yandex.ru>
2023-01-14 22:00:40 +0300

    modules_update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
+  "blockHosts": ["*.google-analytics.com", "*mc.yandex.ru"],
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
